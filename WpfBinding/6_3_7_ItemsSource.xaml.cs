8520ee2d8bf3581188bbc6a722f9fb4e197fd6ff
guo-cheng521 <78196654+guo-cheng521@users.noreply.github.com>
2025-01-02 22:23:17 +0800

    ListBox 和 DataTable

---
+++ WpfBinding/6_3_7_ItemsSource.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfBinding.Model;
+
+namespace WpfBinding
+{
+    /// <summary>
+    /// _6_3_7_ItemsSource.xaml 的交互逻辑
+    /// </summary>
+    public partial class _6_3_7_ItemsSource : Window
+    {
+        public _6_3_7_ItemsSource()
+        {
+            InitializeComponent();
+            List<Student> stuList = new List<Student>()
+            {
+                new Student() {Id = 0, Name = "Tim", Age=29},
+                new Student() {Id = 1, Name = "Tom", Age=28},
+                new Student() {Id = 2, Name = "Kyle", Age=27},
+                new Student() {Id = 3, Name = "Tony", Age=26},
+                new Student() {Id = 4, Name = "Vina", Age=25},
+            };
+
+            this.listBoxStudents.ItemsSource = stuList;
+            //以DisplayMemberPath的值为Path创建Binding，Binding的目标是ListBoxItem的内容插件
+            //this.listBoxStudents.DisplayMemberPath = "Name";
+
+            Binding binding = new Binding("SelectedItem.Id") { Source = this.listBoxStudents };
+            this.textBoxId.SetBinding(TextBox.TextProperty, binding);
+        }
+    }
+}
---
+++ WpfBinding/6_3_8_DataView.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfBinding.Model;
+
+namespace WpfBinding
+{
+    /// <summary>
+    /// 6_3_8_DataView.xaml 的交互逻辑
+    /// </summary>
+    public partial class _6_3_8_DataView : Window
+    {
+        public _6_3_8_DataView()
+        {
+            InitializeComponent();
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Id", typeof(int));
+            dt.Columns.Add("Name", typeof(string));
+            dt.Columns.Add("Age", typeof(int));
+            DataRow dr = dt.NewRow();
+            dr.ItemArray = new object[] { 0, "Tim", 29 };
+            dt.Rows.Add(dr);
+            dr = dt.NewRow();
+            dr.ItemArray = new object[] { 1, "Tom", 28 };
+            dt.Rows.Add(dr);
+            dr = dt.NewRow();
+            dr.ItemArray = new object[] { 2, "Kyle", 27 };
+            dt.Rows.Add(dr);
+            
+
+            this.listViewStudents.ItemsSource = dt.DefaultView;
+
+        }
+    }
+}
