38694c13486d30857a9cf144f44cda4567e11fa5
guo-cheng521 <78196654+guo-cheng521@users.noreply.github.com>
2025-01-06 21:02:44 +0800

    DataConvert

---
+++ WpfBinding/6_4_2_DataConvert.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfBinding
+{
+    /// <summary>
+    /// _6_4_2_DataConvert.xaml 的交互逻辑
+    /// </summary>
+    public partial class _6_4_2_DataConvert : Window
+    {
+        public _6_4_2_DataConvert()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonLoad_Click(object sender, RoutedEventArgs e)
+        {
+            List<Plane> planeList = new List<Plane>()
+            {
+                new Plane() {Category=Category.Bomber, Name="B-1", State=State.Unkonwn },
+                new Plane() {Category=Category.Fighter, Name="F-22", State=State.Unkonwn },
+                new Plane() {Category=Category.Bomber, Name="B-52", State=State.Unkonwn }
+
+            };
+            this.listBoxPlane.ItemsSource = planeList;
+        }
+
+        private void buttonSave_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (Plane p in listBoxPlane.Items)
+            {
+                sb.AppendLine(string.Format("Category={0},Name={1},State={2}", p.Category, p.Name, p.State));
+            }
+            File.WriteAllText(@".\PlaneList.txt", sb.ToString());
+        }
+    }
+    public enum Category
+    {
+        Bomber,
+        Fighter
+    }
+
+    public enum State
+    {
+        Available,
+        Locked,
+        Unkonwn
+    }
+
+    public class Plane
+    {
+        public Category Category { get; set; }
+        public string Name { get; set; }
+        public State State { get; set; }
+    }
+
+    public class CategoryToSourceConverter : IValueConverter
+    {
+        //将Category转换为uri
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Category c = (Category)value;
+            switch (c)
+            {
+                case Category.Bomber:
+                    return @"\Icons\Bomber.png";
+                case Category.Fighter:
+                    return @"\Icons\Fighter.png";
+                default:
+                    return null;
+            }
+        }
+
+        //不会被调用
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class StateToNullableBoolConverter : IValueConverter
+    {
+        //State转换为bool?
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            State s = (State)value;
+            switch (s)
+            {
+                case State.Available:
+                    return true;
+                case State.Locked:
+                    return false;
+                case State.Unkonwn:
+                default:
+                    return null;
+
+            }
+        }
+
+        //将bool?转换为State
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool? nb = (bool?)value;
+            switch (nb)
+            {
+                case true:
+                    return State.Available;
+                case false:
+                    return State.Locked;
+                case null:
+                default:
+                    return State.Unkonwn;
+            }
+        }
+    }
+}
---
+++ WpfBinding/6_5_MultiBinding.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfBinding
+{
+    /// <summary>
+    /// _6_5_MultiBinding.xaml 的交互逻辑
+    /// </summary>
+    public partial class _6_5_MultiBinding : Window
+    {
+        public _6_5_MultiBinding()
+        {
+            InitializeComponent();
+            SetMultiBinding();
+        }
+
+        private void SetMultiBinding()
+        {
+            Binding b1 = new Binding("Text") { Source = this.textBox1 };
+            Binding b2 = new Binding("Text") { Source = this.textBox2 };
+            Binding b3 = new Binding("Text") { Source = this.textBox3 };
+            Binding b4 = new Binding("Text") { Source = this.textBox4 };
+
+            MultiBinding mb = new MultiBinding() { Mode = BindingMode.OneWay };
+            // 对添加子Binding的顺序是敏感的，顺序决定了汇集到Converter里数据的顺序
+            mb.Bindings.Add(b1);
+            mb.Bindings.Add(b2);
+            mb.Bindings.Add(b3);
+            mb.Bindings.Add(b4);
+            //MultiBinding的Converter实现的是IMultiValueConverter接口
+            mb.Converter = new LogonMultiBindingConverter();
+
+            this.button1.SetBinding(Button.IsEnabledProperty, mb);
+        }
+    }
+
+    public class LogonMultiBindingConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!values.Cast<string>().Any(text => string.IsNullOrEmpty(text))
+                && values[0].ToString() == values[1].ToString()
+                && values[2].ToString() == values[3].ToString())
+            {
+                return true;
+            }
+            return false;
+        }
+
+        //不会被调用
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
