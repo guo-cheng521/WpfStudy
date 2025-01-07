691f50509e0e5490585849aa0b63341e02c70769
guo-cheng521 <78196654+guo-cheng521@users.noreply.github.com>
2025-01-07 22:05:16 +0800

    DependencyObject、DependencyProperty

---
+++ WpfProperty/7_2_2_DependencyProperty.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfProperty
+{
+    /// <summary>
+    /// _7_2_2_DependencyProperty.xaml 的交互逻辑
+    /// </summary>
+    public partial class _7_2_2_DependencyProperty : Window
+    {
+        Student stu;
+        public _7_2_2_DependencyProperty()
+        {
+            InitializeComponent();
+            stu = new Student();
+            Binding binding = new Binding("Text") { Source = textBox1 };
+            stu.SetBinding(Student.NameProperty, binding);
+            textBox2.SetBinding(TextBox.TextProperty, new Binding("Name") { Source = stu});
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Student stu = new Student();
+            //stu.SetValue(Student.NameProperty, this.textBox1.Text);
+            //textBox2.Text = (string)stu.GetValue(Student.NameProperty);
+            MessageBox.Show(stu.GetValue(Student.NameProperty).ToString());
+        }
+    }
+}
---
+++ WpfProperty/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WpfProperty
+{
+    internal class Student : DependencyObject
+    {
+        //CLR属性包装器
+        //依赖对象准备了用于暴漏数据的Binding Path，现在依赖对象已经具备了扮演数据源和数据目标双重角色的能力
+        //尽管没有实现INotifyPropertyChanged接口，当属性值发生改变时，与之关联的Binding对象依然可以得到通知，
+        //依赖属性默认带有这样的功能。
+        public string Name
+        {
+            get { return (string)GetValue(NameProperty); }
+            set { SetValue(NameProperty, value); }
+        }
+
+        public static readonly DependencyProperty NameProperty =
+            DependencyProperty.Register("Name", typeof(string), typeof(Student));
+
+        public BindingExpressionBase SetBinding(DependencyProperty dp, BindingBase binding)
+        {
+            return BindingOperations.SetBinding(this, dp, binding);
+        }
+    }
+}
