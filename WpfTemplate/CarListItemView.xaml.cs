6dc19f8096c033c022b1a6c0ab92328b2c6986c1
guo-cheng521 <78196654+guo-cheng521@users.noreply.github.com>
2025-01-16 22:42:38 +0800

    template

---
+++ WpfTemplate/CarListItemView.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTemplate
+{
+    /// <summary>
+    /// CarListItemView.xaml 的交互逻辑
+    /// </summary>
+    public partial class CarListItemView : UserControl
+    {
+        private Car car;
+        public Car Car
+        {
+            get { return car; }
+            set
+            {
+                car = value;
+                this.textBlockName.Text = car.Name;
+                this.textBlockYear.Text = car.Year;
+                string uriStr = string.Format(@"/Resources/Logos/{0}.png", car.Automaker);
+                this.imageLogo.Source = new BitmapImage(new Uri(uriStr, UriKind.Relative));
+            }
+        }
+        public CarListItemView()
+        {
+            InitializeComponent();
+        }
+    }
+}
